85656ac31bb93ac1026ba34644672269463ab8f9
itluobo <121167290@qq.com>
2020-01-12 11:01:53 +0800

    add

---
+++ FilePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FilePath
+{
+    
+    public class FilePath
+    {
+        static int Main()
+        {
+            File.WriteAllText("fileName1", "我.txt");
+            File.WriteAllBytes("fileName", Encoding.GetEncoding("gbk").GetBytes("我.txt"));
+            return 0;
+        }
+    }
+}
